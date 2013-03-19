7de157002a13f540a94bab43ee0fb5aa077fc423
shibayan <shiba-yan@outlook.com>
2013-03-19 15:58:51 +0900

    Contains 属性と SelectList の拡張メソッドを追加

---
+++ SwissKnife/ContainsAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SwissKnife
+{
+    public class ContainsAttribute : ValidationAttribute
+    {
+        public ContainsAttribute(Type type, string propertyName)
+        {
+            _type = type;
+            _propertyName = propertyName;
+        }
+
+        private readonly Type _type;
+        private readonly string _propertyName;
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var selectList = (IEnumerable<SelectListItem>)_type.GetProperty(_propertyName).GetValue(null);
+
+            if (selectList == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var tempValue = value.ToString();
+
+            if (selectList.All(p => p.Value != tempValue))
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ SwissKnife/SelectListExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SwissKnife
+{
+    public static class SelectListExtensions
+    {
+        public static IList<SelectListItem> ToSelectList<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> source)
+        {
+            return source.Select(p => new SelectListItem
+            {
+                Value = p.Key.ToString(),
+                Text = p.Value.ToString()
+            }).ToList();
+        }
+    }
+}
