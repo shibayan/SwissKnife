28b9ec657032934587a073bb39076525d334c82d
shibayan <shiba-yan@outlook.com>
2013-03-20 20:31:05 +0900

    OutputCacheEx 属性と NoOutputCache 属性を追加

---
+++ SwissKnife.Mvc/NoOutputCacheAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SwissKnife.Mvc
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public sealed class NoOutputCacheAttribute : Attribute
+    {
+    }
+}
---
+++ SwissKnife.Mvc/OutputCacheExAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace SwissKnife.Mvc
+{
+    public class OutputCacheExAttribute : OutputCacheAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var skipCaching = filterContext.ActionDescriptor.IsDefined(typeof(NoOutputCacheAttribute), true)
+                              || filterContext.ActionDescriptor.ControllerDescriptor.IsDefined(typeof(NoOutputCacheAttribute), true);
+
+            if (skipCaching)
+            {
+                return;
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
