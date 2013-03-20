dcb6390bc435ea382fe6bfe0ed18b3134fbf2e2a
shibayan <shiba-yan@outlook.com>
2013-03-20 22:32:39 +0900

    PostalCode 属性を追加

---
+++ SwissKnife.Mvc/PostalCodeAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace SwissKnife.Mvc
+{
+    public class PostalCodeAttribute : DataTypeAttribute
+    {
+        public PostalCodeAttribute()
+            : base(DataType.PostalCode)
+        {
+            ErrorMessage = "";
+        }
+
+        private static readonly Regex _regex = new Regex(@"\d{3}-\d{4}", RegexOptions.Compiled);
+
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+
+            var valueAsString = value as string;
+
+            return valueAsString != null && _regex.IsMatch(valueAsString);
+        }
+    }
+}
