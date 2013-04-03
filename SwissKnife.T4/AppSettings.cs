742fcb3b8c3e0e71a86ad96f513300a6273a6eec
shibayan <shiba-yan@outlook.com>
2013-04-03 11:34:12 +0900

    AppSettings クラスを生成する T4 を追加

---
+++ SwissKnife.T4/AppSettings.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+
+namespace SwissKnife.T4
+{
+    public static class AppSettings
+    {
+        public static class Foo
+        {
+            public static string Bar
+            {
+                get { return ConfigurationManager.AppSettings["Foo.Bar"]; }
+            }
+        }
+
+        public static string Sample
+        {
+            get { return ConfigurationManager.AppSettings["Sample"]; }
+        }
+    }
+}
+
