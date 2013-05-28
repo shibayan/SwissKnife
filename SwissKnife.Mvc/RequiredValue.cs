11b07ddb0a5b41c84dad381403e14fa77a60145b
shibayan <shiba-yan@outlook.com>
2013-05-28 09:48:17 +0900

    RequiredValue 属性を追加

---
+++ SwissKnife.Mvc/RequiredValue.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SwissKnife.Mvc
+{
+    public class RequiredValueAttribute : ValidationAttribute
+    {
+        public RequiredValueAttribute(object requiredValue)
+        {
+            _requiredValue = requiredValue;
+        }
+
+        private readonly object _requiredValue;
+
+        public override bool IsValid(object value)
+        {
+            if (value.GetType() != _requiredValue.GetType())
+            {
+                return false;
+            }
+
+            if (!value.Equals(_requiredValue))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
