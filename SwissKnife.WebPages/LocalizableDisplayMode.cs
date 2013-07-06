97b4029cd5f53e4e680cdc36bc2681bee56fc3e0
shibayan <shiba-yan@outlook.com>
2013-07-06 16:29:55 +0900

    LocalizableDisplayMode 追加

---
+++ SwissKnife.WebPages/LocalizableDisplayMode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Web;
+using System.Web.WebPages;
+
+namespace SwissKnife.WebPages
+{
+    public class LocalizableDisplayMode : IDisplayMode
+    {
+        public bool CanHandleContext(HttpContextBase httpContext)
+        {
+            return true;
+        }
+
+        public DisplayInfo GetDisplayInfo(HttpContextBase httpContext, string virtualPath, Func<string, bool> virtualPathExists)
+        {
+            var transformedFilename = TransformPath(virtualPath, Thread.CurrentThread.CurrentCulture.Name);
+
+            if (transformedFilename != null && virtualPathExists(transformedFilename))
+            {
+                return new DisplayInfo(transformedFilename, this);
+            }
+
+            return null;
+        }
+
+        public string DisplayModeId { get { return "Localizable"; } }
+
+        private string TransformPath(string virtualPath, string suffix)
+        {
+            if (string.IsNullOrEmpty(suffix))
+            {
+                return virtualPath;
+            }
+
+            var extension = Path.GetExtension(virtualPath);
+
+            return Path.ChangeExtension(virtualPath, suffix + extension);
+        }
+    }
+
+}
